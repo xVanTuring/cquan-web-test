aafa5004a383af0820adcd60abda3678ac59fdb8
xVan Turing <xVanTuring@outlook.com>
2019-05-28 14:32:01 +0800

    add vanilla demo
    use random image for webpack-version and vanilla-verson

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ![Preview](https://s2.ax1x.com/2019/05/27/VV7Yge.png)
 # Online
 [cquant-web-test](https://xvanturing.github.io/cquant-web-test/)
+[vanilla-js](https://xvanturing.github.io/cquant-web-test/vanilla.html)
 > Open console to see more info
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import CQuant from 'cquant-web'
 import WASM from "cQuantWASM"
 let cquant = new CQuant(WASM, true)
-cquant.palette("./0.jpg", 10).then((result) => {
+cquant.palette("https://picsum.photos/id/482/1920/1080", 10).then((result) => {
     let container = document.querySelector(".block-container")
     for (let item of result) {
         let box = document.createElement('div')
